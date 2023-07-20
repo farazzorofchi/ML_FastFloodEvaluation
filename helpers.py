f1bacf615652fb814a7eff81699aed164f23a6ce
William <68653853+will-i-amv@users.noreply.github.com>
2023-07-20 14:02:52 -0400

    Remove unused functions

--- helpers.py
+++ helpers.py
@@ -87,34 +87,3 @@ def login_required(f):
             return redirect("/login")
         return f(*args, **kwargs)
     return decorated_function
-
-
-def lookup(symbol):
-    """Look up quote for symbol."""
-
-    # Contact API
-    try:
-        api_key = os.environ.get("API_KEY")
-        response = requests.get(
-            f"https://cloud-sse.iexapis.com/stable/stock/" +
-            f"{urllib.parse.quote_plus(symbol)}/quote?token={api_key}"
-        )
-        response.raise_for_status()
-    except requests.RequestException:
-        return None
-
-    # Parse response
-    try:
-        quote = response.json()
-        return {
-            "name": quote["companyName"],
-            "price": float(quote["latestPrice"]),
-            "symbol": quote["symbol"]
-        }
-    except (KeyError, TypeError, ValueError):
-        return None
-
-
-def usd(value):
-    """Format value as USD."""
-    return f"${value:,.2f}"
